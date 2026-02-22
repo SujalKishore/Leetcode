cf6c2a51121401c6aabf172eda47ee210b67c59b
SujalKishore <131854159+SujalKishore@users.noreply.github.com>
2026-02-22 22:15:35 +0530

    Create 136. Single Number.cpp

---
+++ Easy/136. Single Number.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+        for ( int i = 0; i < n; i++){
+            count ^= nums[i];
+        }
+
+        return count;
+    }
+};
